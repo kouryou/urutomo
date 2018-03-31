6014432893b1f6826a20e6f6135392320e5427b0
kouryou <23131663+kouryou@users.noreply.github.com>
2018-03-31 14:47:59 +0900

    add validation to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,15 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images,
                                 reject_if: proc { |attributes| attributes['image'].blank? }
 
+  validates :random_url, uniqueness: true
+  validate :number_of_item_images
+
   include FriendlyId
   friendly_id :random_url
+
+  def number_of_item_images
+    errors.add(:item_images, "image count is invalid!") if item_images.size == 0 || item_images.size > 4
+  end
 
   before_create do
     self.random_url = SecureRandom.hex(10)
